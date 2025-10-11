485aed37527b7e9b20e1b7f7440d32307c4d79f7
Infinite-mind-power <eemami@ucsd.edu>
2025-10-10 17:07:16 -0700

    first commit snacks

---
+++ src/snacks.ts
@@ -0,0 +1,9 @@
+let snacks:string[] = ["fruit", "vegies", "chips", "protein bars", "churros", "ice cream"];
+
+function printSnack(){
+    
+    for(let i = 0; i<snacks.length; i++)
+    console.log(snacks[i]);
+}
+
+printSnack();
