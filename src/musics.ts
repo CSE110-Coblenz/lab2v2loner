1b20ab6412de41c9608269f2637cdf40a7aeeff2
Infinite-mind-power <eemami@ucsd.edu>
2025-10-16 11:52:23 -0700

    fixing music

--- src/musics.ts
+++
@@ -1,9 +0,0 @@
-let musics:string[] = ["hip", "house", "electronic"];
-
-export function printGenre(){
-    
-    for(let i = 0; i<musics.length; i++)
-    console.log(musics[i]);
-}
-
-printGenre();
