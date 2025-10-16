4a2f10cae2b81c36b1e9404d2ff60a796736372b
Elyas Emami <148285115+elyasemami@users.noreply.github.com>
2025-10-16 12:10:29 -0700

    Delete src/music.ts

--- src/music.ts
+++
@@ -1,7 +0,0 @@
-let music:string[] = ["fruit", "vegies", "chips", "protein bars", "churros", "ice cream"];
-
-function printMusic(){
-    
-    for(let i = 0; i<music.length; i++)
-    console.log(music[i]);
-}
